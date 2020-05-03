7ed2f77c966fe1b1c7d2c950e2a462aa5ede3b42
Sam Byass <r3byass@ags.bucks.sch.uk>
2020-05-03 21:34:58 +0100

    Initial commit

---
+++ BotMain.py
@@ -0,0 +1,28 @@
+import discord, os
+import speech_recognition as sr
+
+
+# Speech recognition API stuffs: https://github.com/Uberi/speech_recognition/blob/master/speech_recognition/__main__.py
+
+class BotClient(discord.Client):
+    def __init__(self, **options):
+        super().__init__(**options)
+        self.r = sr.Recognizer()
+
+    async def on_ready(self):
+        print("Logged on as", self.user)
+    # enddef
+
+    async def on_message(self, message: discord.Message):
+        if message.author == self.user:
+            return
+
+        if message.content == "ping":
+            await message.channel.send("pong")
+        # endif
+    # enddef
+# endclass
+
+
+client = BotClient()
+client.run(os.getenv("DISCORD_TOKEN"))
