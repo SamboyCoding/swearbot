47966d549213868d15598e3a94748d137fe13e22
Scramocell <aidantpurchase@gmail.com>
2020-05-25 17:27:04 +0100

    Add docstrings to all methods

--- NaughtyList.py
+++ NaughtyList.py
@@ -7,21 +7,25 @@ class NaughtyList:
     instance = None
 
     def __init__(self):
+        """Connect to naughty_list database. Create a new database if it does not exist."""
         print("Connecting to local database file naughty_list.sqlite...")
         self.db = sqlite3.connect("naughty_list.sqlite")
         self.db.execute("CREATE TABLE IF NOT EXISTS swear_count (id INTEGER PRIMARY KEY, count INTEGER)")
         print("Ready.")
 
     def create_user(self, member: discord.Member):
+        """Add new user, with no swears, into database."""
         self.db.execute("INSERT INTO swear_count(id, count) VALUES (?, ?)", [member.id, 0])
         self.db.commit()
 
     def set_user_score(self, member: discord.Member, score: int):
+        """Update a given discord members swear count."""
         id: int = member.id
         self.db.execute("UPDATE swear_count SET count = ? WHERE id = ?", [score, id])
         self.db.commit()
 
     def get_user_score(self, member: discord.Member) -> int:
+        """Return a given user's swear count. Create a new user if they do not exist."""
         id: int = member.id
         cur = self.db.cursor()
         cur.execute("SELECT count FROM swear_count WHERE id = ?", [id])
@@ -35,6 +39,7 @@ class NaughtyList:
         return row[0]
 
     def get_top_10(self):
+        """Return the ten highest scored members."""
         cur = self.db.cursor()
         cur.execute("SELECT * FROM swear_count ORDER BY count DESC LIMIT 10")
         results: List[Tuple[int, int]] = cur.fetchall()
--- UserHandler.py
+++ UserHandler.py
@@ -11,6 +11,7 @@ from Constants import Constants
 class UserHandler:
 
     def __init__(self, user: discord.Member, base_dir: str):
+        """Define the user, their discord guild, path and setup speech recognition"""
         guild: discord.Guild = user.guild
         self.guild = guild
         self.user = user
@@ -22,6 +23,7 @@ class UserHandler:
         # print("Initialized new user handler for", user, "that logs to file", self.base_path)
 
     def receive_audio(self, pcm_data: bytes):
+        """Reset the silence count and add received audio data to the buffer."""
         # print("Got", len(pcm_data), "bytes audio data for", self.user, "in", self.guild.name)
         if self.consecutive_silence_count > 0:
             self.consecutive_silence_count = 0
@@ -29,6 +31,7 @@ class UserHandler:
         self.buffer += pcm_data
 
     async def receive_silence(self):
+        """Write the received audio buffer data to a timestamped user file and reset buffer."""
         self.consecutive_silence_count += 1
         if self.consecutive_silence_count == 30:
             # print("Received 15 silence packets for", self.user, "assuming they're done talking and flushing",
@@ -46,6 +49,8 @@ class UserHandler:
             await self.recognise(path)
 
     async def recognise(self, path: str):
+        """Convert audio file to text file using speech recognition, add swear count for each swear and post text to
+        discord channel."""
         with sr.AudioFile(path) as f:
             audio = self.r.listen(f)
             try:
